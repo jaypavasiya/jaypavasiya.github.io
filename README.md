bfc8f77e62b6161247035c964441401165aa1619
jaypavasiya <82635946+jaypavasiya@users.noreply.github.com>
2022-04-20 14:40:13 +0530

    Revert "Updates"
    This reverts commit ad1b3cd8fef2a58e2cf00a267c70d5be59fc59b4.

---
+++ README.md
@@ -0,0 +1,2 @@
+# jaypavasiya.github.io
+<!--https://dev.to/ionabrabender/creating-a-portfolio-website-using-github-pages-and-react-part-2-16e1 -->
---
+++ src/Page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home_body'>
+      <div className='ComingSoon' id="writing">█ █ █ <span style={{ color: 'black' }}>█ █ █ █ █ █ █ █ █ █ </span>31%
+        <br />&gt; Site Under Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />&gt; Coming Soon  <span id="imleç">█</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
+  )
+}
+
+export default Home
