e19f233c2ca580b4bfdac00bd2342abfddf01859
jaypavasiya <82635946+jaypavasiya@users.noreply.github.com>
2022-04-29 15:40:18 +0530

    start work on profile

--- src/Page/Profile.js
+++ src/Page/Profile.js
@@ -2,12 +2,7 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='Home_body'>
-      <div className='ComingSoon' id="writing">█ █ █ <span style={{ color: 'black' }}>█ █ █ █ █ █ █ █ █ █ </span>31%
-        <br />&gt; Site Under Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <br />&gt; Coming Soon  <span id="imleç">█</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </div>
-    </div>
+    <div className=''></div>
   )
 }
 
