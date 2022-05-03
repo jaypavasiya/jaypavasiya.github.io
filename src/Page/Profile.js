73d1aa4780dba725d5ad02a2cd1e60efff4c8725
jaypavasiya <82635946+jaypavasiya@users.noreply.github.com>
2022-05-03 19:37:49 +0530

    add animation and title

--- src/Page/Profile.js
+++ src/Page/Profile.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
 const Profile = () => {
+  const arr = new Array(500).fill(0)
+  console.log(arr);
   return (
-    <div className=''>
-      <h1>hello</h1>
-    </div>
+    <>
+      <div className='top_Section'>
+        <div>
+          <div className='title'>
+            <h1>JAY PAVASIYA</h1>
+          </div>
+          <div className='sub_title'>
+            <p>Developer</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className='wrap'>
+          {
+
+            arr.map((item, index) => {
+              return <div className='c' key={index}></div>
+            })
+          }
+
+        </div>
+      </div>
+
+    </>
   )
 }
 
